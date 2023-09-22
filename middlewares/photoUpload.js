b1a329bae787ccb45ddab2f6fcbe5f22d64c777e
haristiks <nklharis@gmail.com>
2023-09-22 10:56:41 +0530

    joi, bcrypt, multer, cloudinary implemented

--- middlewares/photoUpload.js
+++ middlewares/photoUpload.js
@@ -1,33 +1,36 @@
 const multer = require("multer");
-const uploader= multer({
-    storage: multer.memoryStorage(),
-    limits: { fileSize: 500000 }
-  });
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  destination:"uploads/"
+});
+const uploade = multer({storage:storage});
 const cloudinary = require("./cloudinary");
 
+const upload = (req, res, next) => {
+  uploade.single("image")(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({ message: err.message });
+    }
 
-const upload = (req,res,next) => {
-   
-    uploader.single("image")(req, res, async (err) => {
-        if (err) {
-          return res.status(400).json({ message: err.message });
-        }
-    
-        if (!req.file) {
-          return res.status(400).json({ message: "No file uploaded." });
-        }
-    
-        try {
-          const result = await cloudinary.uploader.upload(req.file.buffer, {
-            folder: "product-images", // Replace with your desired folder name
-          });
-    
-          req.body.image = result.secure_url;
-          next();
-        } catch (error) {
-          return res.status(500).json({ message: "Error uploading file to Cloudinary." });
-        }
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded." });
+    }
+
+    try {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "product-images", // Replace with your desired folder name
       });
+
+      req.body.image = result.secure_url;
+      next();
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Error uploading file to Cloudinary." });
+    }
+  });
 };
 
 module.exports = upload;
